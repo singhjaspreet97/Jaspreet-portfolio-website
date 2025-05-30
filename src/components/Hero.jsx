8aca494848c41b734c1a4204e6842544bb264b38
singhjaspreet97 <jassi80singh80@gmail.com>
2025-05-30 14:49:05 -0230

    hero Spcing updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop software, user interfaces,<br className="sm:block hidden" />
-            web applications and provide<br className="sm:block hidden" />
+            web applications and provide <br className="sm:block hidden" />
             Technical Support
           </p>
         </div>
